bbc6db697369e4deca15792cbea56d5656df0cf0
bamirade <118993260+bamirade@users.noreply.github.com>
2025-07-30 10:36:19 +0800

    Update sa.ino

--- sa.ino
+++ sa.ino
@@ -1,6 +1,6 @@
 #include <ESP8266WebServer.h>
-#include <DMDESP.h>
-#include <fonts/Mono5x7.h>
+#include <DMD2.h>
+#include <fonts/Arial14.h>
 #include "PageIndex.h"
 
 const char* ssid = "Led Matrix";
@@ -8,23 +8,24 @@ const char* password = "1234567890";
 
 ESP8266WebServer server(80);
 
+// Display config
 #define DISPLAYS_WIDE 5
 #define DISPLAYS_HIGH 1
-DMDESP Disp(DISPLAYS_WIDE, DISPLAYS_HIGH);
+SPIDMD dmd(DISPLAYS_WIDE, DISPLAYS_HIGH); // Use SPI pins (hardware SPI)
 
+// Number of rows
 #define NUM_ROWS 3
 
 // Buffers for each row
 char TextBuffers[NUM_ROWS][100] = {
   "WELCOME TO AMA SANTIAGO", "", ""
 };
-
 char* Text[] = {TextBuffers[0], TextBuffers[1], TextBuffers[2]};
 
-// Scrolling state for each row
+// Scrolling state
 uint32_t scrollPos[NUM_ROWS] = {0, 0, 0};
 uint32_t prevMillis[NUM_ROWS] = {0, 0, 0};
-const uint8_t rowHeights[NUM_ROWS] = {0, 8, 16}; // Y positions for each row
+const uint8_t rowHeights[NUM_ROWS] = {0, 8, 16};
 
 void handleRoot() {
   server.send(200, "text/html", MAIN_page);
@@ -36,11 +37,7 @@ void handle_Incoming_Text() {
     int row = server.arg("row").toInt();
 
     if (row >= 0 && row < NUM_ROWS) {
-      Serial.print("Row ");
-      Serial.print(row);
-      Serial.print(" text: ");
-      Serial.println(incoming);
-
+      Serial.printf("Row %d text: %s\n", row, incoming.c_str());
       incoming.toCharArray(TextBuffers[row], sizeof(TextBuffers[row]));
       server.send(200, "text/plain", "Text received for row " + String(row));
     } else {
@@ -55,17 +52,14 @@ void setup() {
   Serial.begin(115200);
   delay(500);
 
-  Disp.start();
-  Disp.setBrightness(50);
-  Disp.setFont(Mono5x7);
+  dmd.setBrightness(255);
+  dmd.selectFont(Arial14);
+  dmd.begin();
 
   WiFi.softAP(ssid, password);
   IPAddress apip = WiFi.softAPIP();
 
-  Serial.print("Connect to: ");
-  Serial.println(ssid);
-  Serial.print("IP Address: ");
-  Serial.println(apip);
+  Serial.printf("Connect to: %s\nIP Address: %s\n", ssid, apip.toString().c_str());
 
   server.on("/", handleRoot);
   server.on("/setText", handle_Incoming_Text);
@@ -75,25 +69,29 @@ void setup() {
 
 void loop() {
   server.handleClient();
-  Disp.loop();
+  dmd.clearScreen();
 
   for (int i = 0; i < NUM_ROWS; i++) {
-    scrollTextRow(i, rowHeights[i], 50); // Y-pos per row
+    scrollTextRow(i, rowHeights[i], 50);
   }
+
+  delay(30); // adjust for smoothness
 }
 
 void scrollTextRow(int index, int y, uint8_t speed) {
-  int width = Disp.width();
-  Disp.setFont(Mono5x7);
+  int width = dmd.width;
+  dmd.selectFont(Arial14);
 
-  int fullScroll = Disp.textWidth(Text[index]) + width;
+  int fullScroll = dmd.stringWidth(Text[index]) + width;
   if ((millis() - prevMillis[index]) > speed) {
     prevMillis[index] = millis();
+
     if (scrollPos[index] < fullScroll) {
       scrollPos[index]++;
     } else {
       scrollPos[index] = 0;
     }
-    Disp.drawText(width - scrollPos[index], y, Text[index]);
   }
+
+  dmd.drawString(width - scrollPos[index], y, Text[index]);
 }
